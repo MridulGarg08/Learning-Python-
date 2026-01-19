ac9ab0bd974e4673a52904ce7b50cc04c1ad1767
Mridul Garg <mridulgarg8888@gmail.com>
2026-01-19 16:15:41 +0530

    Conditional Statements Done!

---
+++ Begineer/Conditional_Statements.py
@@ -0,0 +1,21 @@
+age=input("Enter your age!")
+age=int(age)
+if(age>25):
+    print("Adult")
+# elif(20<age<=25):
+#     print("Teen")
+elif(20<age and age<=25):   #Logical Operator and in python and && in java
+    print("Teen")
+else:
+    print("Not Considered")
+    
+# price=100000
+# has_good_credit=True
+
+# if(has_good_credit):
+#     down_payment=0.1*price
+# else:
+#     down_payment=0.2*price
+
+# print(down_payment)
+    
--- Begineer/app.py
+++ Begineer/app.py
@@ -1,3 +1,4 @@
+import math
 print("Hello World!!!")
 
 print('*'*10)  #This will draw 10 asterisk but in java we dont have such thing
@@ -58,5 +59,99 @@ print(str[-1]) # This will rerturn as last index if -2 second last then
 first="John"
 last="Smith"
 message=first+" ["+last+"] is a great coder!!"
-# message=f"{first} [{last}] is a great coder!!!"  # Formatted string
+# message=f"{first} [{last}] is a great coder!!!"  # Formatted string used for dynamically inserting values to your string
 print(message)
+
+#String length
+
+string="Python"
+print(len(string))  #In java we use string.length()  in python we just use len()
+
+print(string.upper()) # In java we use .toUpperCase()
+print(string.lower()) # In java we use .toLowerCase()
+
+print(string.find('Y'))  # used to find the inde of particular charactrer in a string.cases are sensitive for Y -1 for y 1 
+print(string.find('tho'))  # In java, .indexOf() is used, works for substring as well
+
+print(string.replace('P','JOY')) # Used to replace the character with some other character or sibstring with other substring same as in java
+
+print("Yth" in string) # used to find if that particular character or string is there in the 'string' , this si also case sensitive
+
+#String functions 
+
+# len() 
+# upper()  
+# .lower()
+# .find()
+# .replace()
+# '...' in string
+
+#Arithmetic Operations
+
+print(10/3) #same as in java but results in Integer in java but in floating type in python
+print(10%3) #same as in java
+print(10*3) #same as in java
+print("hello "*3) #for strings multiply for occurences
+print(10-3) #same as in java
+print(10+3) #same as in java
+print(10**3)  #for power
+x=20
+print(x)
+x+=40  # Works as in java
+print(x)
+
+# Operator Precedence
+y=50+9*5
+print(y)
+#Order
+#Paranthesis --> Exponentiation --> multiplication/division/modulus --> Addition/Substraction
+#If have same precedence then it will be evaluated from left to right
+
+#Rounding Off Operations
+x=2.9
+print(round(x))
+
+print(round(2.3))   # 2
+print(round(2.7))   # 3
+print(round(2.5))   # 2 
+print(round(3.5))   # 4
+
+# Python works similar to java until unless there is .5 in a number #In python if thre is .5 then it is always rounded up to nearest even integer but in java its always up to next in case of .5
+
+#Absolute 
+print(abs(-2.9)) #Always returns a positive number
+
+#Mathematical Functions
+print(math.ceil(2.9))
+print(math.floor(2.9))
+
+#Logical Operators
+
+# and-> both conditions should be true -> && in java
+# or-> atleast one should be true-> || in java
+# not-> flipped value of True means should be false-> ! in java
+
+#Comparison Operators
+
+# > Greater than
+# < Less than
+# >= Greater than or Equal to
+# <= Less than or Equal to
+
+print("Entr Your name")
+name=input()
+
+len=len(name)
+if(len<3):
+    print("Name must be atleast 3 characters long!!!")
+elif(len>50):
+    print("Name can be a maximum of 50 characters!!!")  
+else:
+    print("Name looks good!!!")
+
+
+ 
+
+
+
+
